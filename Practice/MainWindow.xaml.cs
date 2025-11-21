b69bf5b1c88e886dce9a548f9856014ca544ad18
Великий Владимир Павлович <10230902@college.local>
2025-11-21 17:59:23 +0300

    v2

--- Practice/MainWindow.xaml.cs
+++ Practice/MainWindow.xaml.cs
@@ -20,5 +20,69 @@ namespace Practice
         {
             InitializeComponent();
         }
+
+        private void E_mail_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var converter = new BrushConverter();
+            Brush myBrush = (Brush)converter.ConvertFrom("#B70000");
+            E_mail.Foreground = myBrush;
+            E_mail.Opacity = 0.5;
+        }
+
+        private void E_mail_MouseLeave(object sender, MouseEventArgs e)
+        {
+            E_mail.Foreground = Brushes.Black;
+            E_mail.Opacity = 0.13;
+        }
+
+        private void Phone_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var converter = new BrushConverter();
+            Brush myBrush = (Brush)converter.ConvertFrom("#B70000");
+
+            Phone.Foreground = myBrush;
+            Phone.Opacity = 0.5;
+
+        }
+
+        private void Phone_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Phone.Foreground = Brushes.Black;
+            Phone.Opacity = 0.13;
+        }
+
+        private void MLBD_Email(object sender, MouseButtonEventArgs e)
+        {
+            var converter = new BrushConverter();
+            Brush myBrush = (Brush)converter.ConvertFrom("#B70000");
+            E_mail.Foreground = myBrush;
+            Phone.Foreground = Brushes.Black;
+            E_P_Label.Content = "E-mail";
+        }
+
+        private void MLBD_Phone(object sender, MouseButtonEventArgs e)
+        {
+            var converter = new BrushConverter();
+            Brush myBrush = (Brush)converter.ConvertFrom("#B70000");
+            Phone.Foreground = myBrush;
+            E_mail.Foreground = Brushes.Black;
+            E_P_Label.Content = "Номер телефона";
+        }
+
+
+
+        private void AlreadyAccount_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var converter = new BrushConverter();
+            Brush myBrush = (Brush)converter.ConvertFrom("#B70000");
+            AlreadyAccount.Foreground = myBrush;
+            AlreadyAccount.Opacity = 0.5;
+        }
+
+        private void AlreadyAccount_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AlreadyAccount.Foreground = Brushes.Black;
+            AlreadyAccount.Opacity = 0.13;
+        }
     }
 }
